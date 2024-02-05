762ba9b777f9b7f56c3e76b222ff50ffb4f78b19
nekono <91360587+nkte8@users.noreply.github.com>
2024-02-06 01:08:57 +0900

    QAを追加しました。

--- astro/src/utils/vars.ts
+++ astro/src/utils/vars.ts
@@ -1,10 +1,11 @@
 export const baseurl = import.meta.env.BASE_URL
 export const siteurl = import.meta.env.SITE
-export const github_url = "https://github.com/nkte8/skyshare"
-export const zenn_profile = "https://zenn.dev/nkte8"
-export const zenn_url = "https://zenn.dev/nkte8/articles/2024-02-03-r01"
+export const githuburl = "https://github.com/nkte8/skyshare"
+export const zennprofile = "https://zenn.dev/nkte8"
+export const zennurl = "https://zenn.dev/nkte8/articles/2024-02-03-r01"
 export const posturl = baseurl + "app/"
 export const abouturl = baseurl + "about/"
+export const qaurl = baseurl + "qa/"
 export const pagesPrefix = "posts"
 export const servicename = "Skyshare"
-export const version = "v1.0.5"
+export const version = "v1.0.7"
