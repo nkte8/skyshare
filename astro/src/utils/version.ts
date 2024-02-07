75e9bd0de8297196c5fdaeb9d2405c0044138aa2
nekono <91360587+nkte8@users.noreply.github.com>
2024-02-08 01:36:23 +0900

    Cloudflare側でPathが解決できていないため、ハードリンクを追加

--- astro/src/utils/version.ts
+++ astro/src/utils/version.ts
@@ -1,7 +1,7 @@
 // package.jsonから自動でバージョンを取得
 // SSRモードのページでは動かない
 import fs from 'node:fs/promises';
-const url = new URL('../../package.json', import.meta.url);
+const url = new URL('./package.json', import.meta.url);
 const json = await fs.readFile(url, 'utf-8');
 const data = JSON.parse(json);
 export const version = `v${data.version}`
