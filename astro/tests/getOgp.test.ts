f2ff31b36375c8c08a9bb0f0f317194e8a884085
nekono <91360587+nkte8@users.noreply.github.com>
2024-03-05 02:33:27 +0900

    Issue11に関するテストの追加・実施・Changelogの更新実施

--- astro/tests/getOgp.test.ts
+++ astro/tests/getOgp.test.ts
@@ -1,15 +1,15 @@
 import { describe, test, expect } from "@jest/globals";
 import { getOgpMeta } from "../src/utils/getOgp"
-import type { ogpMataData, errorResponse } from "../src/lib/types";
+import type { ogpMetaData, errorResponse } from "../src/lib/types";
 
 const version = `v${process.env.PUBLIC_VERSION}`
 const siteurl = "http://192.168.3.200:4321"
 
 describe('getOgp Test', () => {
     test('True Website test', async () => {
-        let result: ogpMataData | errorResponse = await getOgpMeta(siteurl, "https://skyshare.uk")
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "https://skyshare.uk")
         expect(result).toEqual(
-            <ogpMataData>{
+            <ogpMetaData>{
                 type: "meta",
                 title: "Skyshare - Share Bluesky to X",
                 description: "Skyshare is a web application helps bluesky users post to both bluesky or x.com.",
@@ -19,9 +19,9 @@ describe('getOgp Test', () => {
     }, 10000) // long timeouf)
     // YoutubeはCORSが設定されている例
     test('True Website cors site test', async () => {
-        let result: ogpMataData | errorResponse = await getOgpMeta(siteurl, "https://www.youtube.com/watch?v=xitQ_oNTVvE")
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "https://www.youtube.com/watch?v=xitQ_oNTVvE")
         expect(result).toEqual(
-            <ogpMataData>{
+            <ogpMetaData>{
                 type: "meta",
                 title: "Google Chrome スピードテスト",
                 description: "20秒でできる？Google Chromeブラウザでお出かけ前の情報チェック。google.co.jp/chrome",
@@ -31,9 +31,9 @@ describe('getOgp Test', () => {
     }, 100000) // long timeouf)
     // Zennは　twitter:imageが設定されていない例
     test('True Website no twitter:image test', async () => {
-        let result: ogpMataData | errorResponse = await getOgpMeta(siteurl, "https://zenn.dev")
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "https://zenn.dev")
         expect(result).toEqual(
-            <ogpMataData>{
+            <ogpMetaData>{
                 type: "meta",
                 title: "Zenn｜エンジニアのための情報共有コミュニティ",
                 description: "Zennはエンジニアが技術・開発についての知見をシェアする場所です。本の販売や、読者からのバッジの受付により対価を受け取ることができます。",
@@ -43,13 +43,53 @@ describe('getOgp Test', () => {
     }, 100000) // long timeouf)
     // It media はエンコーディングが古い shift-jisのサイト
     test('True Website shift-jis site test', async () => {
-        let result: ogpMataData | errorResponse = await getOgpMeta(siteurl, "https://www.itmedia.co.jp/news/")
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "https://www.itmedia.co.jp/news/")
         expect(result).toEqual(
-            <ogpMataData>{
+            <ogpMetaData>{
                 type: "meta",
                 title: "ITmedia NEWS",
                 description: "ITがもたらす変化を敏感に感じ取り、仕事や生活に生かしていこうという企業内個人やネットサービス開発者、ベンチャー経営者をコアターゲットに、IT業界動向やネットサービストレンド、ネット上の話題までカバーするニュースを配信します。",
                 image: `https://image.itmedia.co.jp/images/logo/1200x630_500x500_news.gif`
+            }
+        )
+    }, 100000) // long timeouf)
+    test('Bad request localhost test', async () => {
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "http://localhost/")
+        expect(result).toEqual(
+            <errorResponse>{
+                type: "error",
+                error: "Bad Gateway@getOgpMeta",
+                message: "Failed to get correct response from gateway. Announce server administrator."
+            }
+        )
+    }, 100000) // long timeouf)
+    test('Bad request no dns ipv4 website test', async () => {
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "http://192.168.0.2/")
+        expect(result).toEqual(
+            <errorResponse>{
+                type: "error",
+                error: "Bad Gateway@getOgpMeta",
+                message: "Failed to get correct response from gateway. Announce server administrator."
+            }
+        )
+    }, 100000) // long timeouf)
+    test('Bad request no dns ipv6 website test', async () => {
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "http://[fe00::1]/")
+        expect(result).toEqual(
+            <errorResponse>{
+                type: "error",
+                error: "Bad Gateway@getOgpMeta",
+                message: "Failed to get correct response from gateway. Announce server administrator."
+            }
+        )
+    }, 100000) // long timeouf)
+    test('Bad request invalid protocol test', async () => {
+        let result: ogpMetaData | errorResponse = await getOgpMeta(siteurl, "file://./index.html")
+        expect(result).toEqual(
+            <errorResponse>{
+                type: "error",
+                error: "Bad Gateway@getOgpMeta",
+                message: "Failed to get correct response from gateway. Announce server administrator."
             }
         )
     }, 100000) // long timeouf)
