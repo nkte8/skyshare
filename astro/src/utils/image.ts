b30a0cbfc54df27064c040dd2aa823a509c23e82
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-03-14 22:25:39 +0900

    ♻️ 画像追加処理共通化のため、既存実装をリファクタリング

---
+++ astro/src/utils/image.ts
@@ -0,0 +1,37 @@
+import { compressImage } from "@/utils/compressimage"
+import { Dispatch, SetStateAction } from "react"
+
+const maxAttachableImages = 4
+const allowedMimeTypes: string[] = ["image/png", "image/jpeg"]
+const imageExtensions: string[] = ["png", "jpeg", "jpg"]
+
+/**
+ *
+ * @param newFiles 新規に追加されたファイル配列
+ * @param imageFiles 既存の画像ファイル配列
+ * @param setImageFile 画像ファイル配列のセッター
+ */
+const addImages = async (
+    newFiles: File[],
+    imageFiles: File[],
+    setImageFile: Dispatch<SetStateAction<File[]>>,
+): Promise<void> => {
+    const existingImageFiles: File[] = imageFiles.length > 0 ? imageFiles : []
+    const additionalImageFiles: Promise<File>[] = []
+
+    newFiles.forEach((file: File) => {
+        if (allowedMimeTypes.includes(file.type)) {
+            additionalImageFiles.push(compressImage(file))
+        }
+    })
+
+    const concatenatedFiles: File[] = existingImageFiles.concat(
+        await Promise.all<File>(additionalImageFiles),
+    )
+
+    const resultFiles: File[] = concatenatedFiles.slice(0, maxAttachableImages)
+
+    setImageFile(resultFiles)
+}
+
+export { imageExtensions, addImages }
