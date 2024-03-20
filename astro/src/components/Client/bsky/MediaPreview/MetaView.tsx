5b0b0919e2e76d59f6f05e4928643ce3a1e65df4
nekono <91360587+nkte8@users.noreply.github.com>
2024-03-20 20:28:26 +0900

    ビューにメタタグ情報を追加

---
+++ astro/src/components/Client/bsky/MediaPreview/MetaView.tsx
@@ -0,0 +1,37 @@
+// utils
+import React, { memo, useRef, useCallback, useState, useEffect } from "react";
+
+// service
+import { MediaData } from "../../common/types"
+
+export const Component = ({
+    mediaData
+}: {
+    mediaData: MediaData
+}) => {
+    // mediaDataがimageではない場合はコンポーネントを無効に
+    if (mediaData === null || mediaData.type !== "external") {
+        return
+    }
+    let mediaDataItem = mediaData.meta
+    return (
+        <div className={[
+            "rounded-2xl", "border-2",
+            "bg-opacity-90", "p-2",
+            "bg-white", "border-gray-200",
+            "text-black", "absolute",
+            "left-0", "bottom-0",
+            "w-full", "m-0"
+        ].join(" ")}>
+            <div className={["text-left", "m-0"].join(" ")}>
+                {mediaDataItem.title}
+            </div>
+            <div className={[
+                "text-left", "m-0", "text-xs",
+                "text-gray-600"].join(" ")}>
+                {mediaDataItem.description}
+            </div>
+        </div>
+    );
+};
+export default Component
