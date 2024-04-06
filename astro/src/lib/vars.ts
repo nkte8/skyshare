08cb5fef13a1267b3f0c544d1ac48314fee7a1dd
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-04-06 12:46:32 +0000

    🚨 nullish判定を明確化

--- astro/src/lib/vars.ts
+++ astro/src/lib/vars.ts
@@ -1,6 +1,6 @@
 // 正しくbool値を処理。 `True` 以外は全て False として処理
 const stringToBoolean = (value: string | null | undefined): boolean => {
-    if (!value) {
+    if (value == null) {
         return false;
     }
     return value.toLowerCase() === 'true';
