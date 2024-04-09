e8e1b978eab07dc9d46fd797c08cfdd93c6d255a
nekono <91360587+nkte8@users.noreply.github.com>
2024-04-09 20:28:23 +0900

    言語対応

--- astro/src/components/Client/bsky/selectLists/LanguageSelectList.tsx
+++ astro/src/components/Client/bsky/selectLists/LanguageSelectList.tsx
@@ -1,41 +1,57 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction } from "react"
 import SelectList from "../../common/SelectList"
 const langList = [
     {
         label: "日本語",
-        code: "ja"
-    }, {
+        code: "ja",
+    },
+    {
         label: "English",
-        code: "en"
-    }, {
+        code: "en",
+    },
+    {
         label: "中文",
-        code: "zh"
-    }, {
+        code: "zh",
+    },
+    {
         label: "한국어",
-        code: "ko"
-    }, {
+        code: "ko",
+    },
+    {
         label: "Polski",
-        code: "pl"
-    }, {
+        code: "pl",
+    },
+    {
         label: "Portuguese",
-        code: "pt"
-    }, {
+        code: "pt",
+    },
+    {
         label: "Deutsch",
-        code: "de"
-    }];
+        code: "de",
+    },
+    {
+        label: "Français",
+        code: "fr",
+    },
+    {
+        label: "हिन्दी",
+        code: "hi",
+    },
+]
 export const Component = ({
     disabled,
-    setLanguage
+    setLanguage,
 }: {
-    disabled: boolean,
-    setLanguage: Dispatch<SetStateAction<string>>,
+    disabled: boolean
+    setLanguage: Dispatch<SetStateAction<string>>
 }) => {
     return (
         <>
-            <SelectList 
+            <SelectList
                 setCode={setLanguage}
                 codeMap={langList}
-                disabled={disabled}/>
+                disabled={disabled}
+            />
         </>
     )
 }
