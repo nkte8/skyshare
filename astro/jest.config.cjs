b38ac1d67ffd988eb3b6371e8e89f75361db9204
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-05-02 17:40:49 +0900

    ⏪ 不要になった変更を元の状態に復元

--- astro/jest.config.cjs
+++ astro/jest.config.cjs
@@ -1,8 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1",
-    },
-}
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+};
